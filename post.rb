9763245caa82e8fc478d8581ef06a3fb685adf69
Dmitriy <tds050680@gmail.com>
2016-10-08 09:21:08 +0300

    initial

---
+++ post.rb
@@ -0,0 +1,20 @@
+class Post
+
+  def initialize
+    @created_at = Time.now
+    @text = ""
+  end
+
+  def read_from_console
+    #to do
+  end
+
+  def to_strings
+    #to do
+  end
+
+  def save
+
+  end
+
+end
